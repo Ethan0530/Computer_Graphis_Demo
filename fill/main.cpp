eea4981ad3f2c311510b0e156a4fe631ef7e9a85
HAL101010 <3218497724@qq.com>
2023-04-25 19:01:32 +0800

    modified:   fill/main.cpp

--- fill/main.cpp
+++ fill/main.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <cmath>
 #include <graphics.h>
+#define MAX_SIZE 20
 
 typedef struct Edge{
     float ymax;//扫描线所交边的最高Y值
@@ -9,6 +10,18 @@ typedef struct Edge{
     float dx;//边斜率的倒数 dx = 1/k
     struct Edge* next;
 }Edge;
+
+typedef struct Polygon{
+    int line_num;
+    int x1[MAX_SIZE];
+    int y1[MAX_SIZE];
+    int x2[MAX_SIZE];
+    int y2[MAX_SIZE];
+}Polygon;
+
+void polyfill(Polygon polygon,int Color){
+
+}
 
 int main(){
     initgraph(640,480);
