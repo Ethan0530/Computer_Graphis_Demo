83b30001355b7ebe7df050acb191367ebcc48d3c
HAL101010 <3218497724@qq.com>
2023-04-25 19:35:46 +0800

    modified:   fill/main.cpp

--- fill/main.cpp
+++ fill/main.cpp
@@ -6,17 +6,19 @@
 
 typedef struct Edge{
     float ymax;//扫描线所交边的最高Y值
-    float x;//当期扫描线与边的交点
+    float x;//当前扫描线与边的交点的x值
     float dx;//边斜率的倒数 dx = 1/k
     struct Edge* next;
 }Edge;
 
-typedef struct Polygon{
-    int line_num;
-    int x1[MAX_SIZE];
-    int y1[MAX_SIZE];
-    int x2[MAX_SIZE];
-    int y2[MAX_SIZE];
+//多边形结构体
+typedef struct{
+    int x1,y1,x2,y2;
+}PolygonEdge;
+
+typedef struct{
+    int line_nums;
+    PolygonEdge polygonEdge[MAX_SIZE];
 }Polygon;
 
 void polyfill(Polygon polygon,int Color){
