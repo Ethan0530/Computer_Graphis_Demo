c251f0dd2851b69a000b5c23a6fc4937a1e2e023
HAL101010 <3218497724@qq.com>
2023-05-04 17:14:29 +0800

    modified:   fill/main.cpp

--- fill/main.cpp
+++ fill/main.cpp
@@ -10,9 +10,12 @@
  * b:排序，把所有交点按x坐标递增进行排序
  * c:配对(奇偶性，交点取舍(交点数应为偶数))解决方案:若共享顶点的两条边分别落在扫描线两边，就只算一个交点;若在扫描线同一边，算0个或2个(看这两条边另外两端点y值是大于(2)交点还是小于(0)交点)
  * d:区间填色
+ * 
+ * 构造新边表步骤
+ * a:首先构造一个纵向链表，链表的长度为多边形所占有的最大扫描线数，链表的每一个结点称为一个吊桶，对应多边形覆盖的每一条扫描线
 */
 
-//活性边表(针对一条固定的扫描线来说)
+//活性边表(AET)(针对一条固定的扫描线来说)
 typedef struct Edge{
     float ymax;//与该边所交的的最高扫描线的Y值(从ymin到ymax依次扫描填充)
     float x;//当前边与扫描线的交点的x值
@@ -30,15 +33,35 @@ typedef struct{
     int x1,y1,x2,y2;
 }PolygonEdge;
 
-//多边形结构体
+//点集
 typedef struct{
-    int line_nums;
-    PolygonEdge polygonEdge[MAX_SIZE];
+    int x,y;
+}Point;
+
+//多边形结构体(顶点表示)
+typedef struct{
+    int num;
+    const Point *points;
 }Polygon;
 
+//多边形结构体(边表示)
+typedef struct{
+    int line_num;
+    int *x1;
+    int *y1;
+    int *x2;
+    int *y2;
+}Polygon_Edge;
+
+//顶点表示法的多边形填充
 void polyfill(Polygon polygon,int Color){
+
+}
+
+//边表示法的多边形填充
+void polyfill(Polygon_Edge polygon,int Color){
     //扫描多边形各条线，初始化新边表
-    for(int i = 0;i < polygon.line_nums;i++){
+    for(int i = 0;i < polygon.line_num;i++){
         //初始化新边表头指针NET[i]
         //把ymin = i的边放进新编表NET[i]
     }
