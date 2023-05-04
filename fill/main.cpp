054111b6c6834209c678ed149461290b8f914a9c
HAL101010 <3218497724@qq.com>
2023-05-04 16:07:29 +0800

    modified:   fill/main.cpp

--- fill/main.cpp
+++ fill/main.cpp
@@ -4,8 +4,17 @@
 #include <graphics.h>
 #define MAX_SIZE 20
 
+/**
+ * 对每一条扫描线填充的过程可分为4个步骤
+ * a:求交，计算扫描线与多边形各个边的交点
+ * b:排序，把所有交点按x坐标递增进行排序
+ * c:配对(奇偶性，交点取舍(交点数应为偶数))解决方案:若共享顶点的两条边分别落在扫描线两边，就只算一个交点;若在扫描线同一边，算0个或2个(看这两条边另外两端点y值是大于(2)交点还是小于(0)交点)
+ * d:区间填色
+*/
+
 typedef struct Edge{
-    float ymax;//扫描线所交边的最高Y值
+    float ymax;//扫描线所交边的最高Y值(从ymin到ymax依次扫描填充)
+    float ymin;//扫描线所交边的最低Y值
     float x;//当前扫描线与边的交点的x值
     float dx;//边斜率的倒数 dx = 1/k
     struct Edge* next;
